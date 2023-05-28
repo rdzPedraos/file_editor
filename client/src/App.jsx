0128517be92ed462c95fa61f1adfba5df42c40d1
rdzPedraos <juancursosrodriguez@gmail.com>
2023-05-28 17:34:16 -0500

    modified app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,18 @@
 import { useContext } from 'react';
 
 import Upload from './pages/Upload';
+import GuestLayout from './layouts/guest';
 import { fileTypes } from './utils/fileTypes';
 import { FileContext } from './context/FileContext';
 
 function App() {
-	const { file, setFile } = useContext(FileContext);
+	const { file } = useContext(FileContext);
 	const Component = fileTypes[file?.type]?.page ?? Upload;
 
 	return (
-		<div>
-			<button onClick={() => setFile(null)}>Home</button>
-
+		<GuestLayout>
 			<Component />
-		</div>
+		</GuestLayout>
 	);
 }
 
