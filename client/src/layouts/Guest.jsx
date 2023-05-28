dfe4afc9e27831bb79369297b4ab0c867e9f62db
rdzPedraos <juancursosrodriguez@gmail.com>
2023-05-28 18:20:36 -0500

    Upgrade Guest Layout

--- client/src/layouts/Guest.jsx
+++ client/src/layouts/Guest.jsx
@@ -7,17 +7,17 @@ import svg from '../img/bg.svg';
 function GuestLayout({ children }) {
 	return (
 		<div className='min-w-screen min-h-screen bg-base-light'>
-			<Box Type='header' className='flex h-24 items-center'>
+			<Box Type='header' className='flex relative z-10 h-24 items-center'>
 				<Logo size='md' />
 			</Box>
 
-			<main className='relative min-h-[calc(100vh-96px)] w-screen'>
+			<main className='relative z-10 min-h-[calc(100vh-96px)] w-screen'>
 				{children}
-
-				<div className='absolute top-0 right-0 bottom-0 left-0'>
-					<img src={svg} className='w-full h-full object-cover' />
-				</div>
 			</main>
+
+			<div className='absolute top-0 right-0 bottom-0 left-0'>
+				<img src={svg} className='w-full h-full object-cover' />
+			</div>
 		</div>
 	);
 }
