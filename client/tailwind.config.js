e53779a3aa53f6777d19f91f4b0d1b30f8bde033
rdzPedraos <juancursosrodriguez@gmail.com>
2023-05-28 16:39:58 -0500

    set standar styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				base: 'rgb( var(--base) / <alpha-value> )',
+				'base-light': 'rgb( var(--base-light) / <alpha-value> )',
+
+				primary: 'rgb( var(--primary) / <alpha-value> )',
+				'primary-light': 'rgb( var(--primary-light) / <alpha-value> )',
+
+				seconadry: 'rgb( var(--seconadry) / <alpha-value> )',
+				'secondary-light': 'rgb( var(--secondary-light) / <alpha-value> )',
+
+				info: 'rgb( var(--info) / <alpha-value> )',
+				'info-light': 'rgb( var(--info-light) / <alpha-value> )',
+			},
+		},
 	},
 	plugins: [],
 };
