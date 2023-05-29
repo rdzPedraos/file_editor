d91740b1aca8ecc3a7897a3cd0470c778b261907
rdzPedraos <juancursosrodriguez@gmail.com>
2023-05-29 02:08:24 -0500

    Set variables for Button Component

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -6,14 +6,26 @@ const styles = {
 	info: 'bg-info-light text-info',
 };
 
-function Button({ children, style = 'primary', className = '', ...props }) {
+const sizes = {
+	sm: 'px-2 text-sm',
+	md: 'py-2 px-3 text-lg',
+	null: '',
+};
+
+function Button({
+	children,
+	size = 'md',
+	style = 'primary',
+	className = '',
+	...props
+}) {
 	return (
 		<button
 			className={`
-                px-3 py-2 rounded font-medium text-lg
+                rounded font-medium
                 transition-all hover:bg-opacity-50 hover:shadow
                 active:-translate-y-1 active:shadow-none
-                ${styles[style]} ${className}
+                ${styles[style]} ${sizes[size]} ${className}
             `}
 			{...props}
 		>
@@ -26,6 +38,7 @@ Button.propTypes = {
 	children: PropTypes.node,
 	className: PropTypes.string,
 	style: PropTypes.oneOf(['primary', 'secondary', 'info']),
+	size: PropTypes.oneOf(['sm', 'md', 'lg']),
 };
 
 export default Button;
