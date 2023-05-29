abf7f368ce3c0d6be681c188b61ab9088295ffc9
rdzPedraos <juancursosrodriguez@gmail.com>
2023-05-28 19:09:16 -0500

    Added attributes in File Context

--- client/src/context/FileContext.jsx
+++ client/src/context/FileContext.jsx
@@ -7,16 +7,27 @@ export const FileContext = createContext();
 function FileProvider({ children }) {
 	const [file, setFile] = useState(null);
 	const [data, setData] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
 
 	useEffect(() => {
 		if (file === null || file === undefined) return;
+		setLoading(true);
 
-		const data = fileTypes[file.type]?.extractData(file);
-		setData(data);
+		const fileType = fileTypes[file?.type];
+
+		if (fileType) {
+			const data = fileType.extractData(file);
+			setData(data);
+			setError(null);
+		} else {
+			setError('No se reconoce el tipo de archivo.');
+		}
+		setLoading(false);
 	}, [file]);
 
 	return (
-		<FileContext.Provider value={{ data, file, setFile }}>
+		<FileContext.Provider value={{ data, file, setFile, loading, error }}>
 			{children}
 		</FileContext.Provider>
 	);
