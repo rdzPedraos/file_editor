ee6d29e6f5f2659d00a95d25151f579fda65a2d7
rdzPedraos <juancursosrodriguez@gmail.com>
2023-05-28 19:48:52 -0500

    Set function for extract csv files

--- client/src/utils/fileTypes.js
+++ client/src/utils/fileTypes.js
@@ -2,11 +2,49 @@ import EditTxt from '../pages/EditTxt';
 import EditCsv from '../pages/EditCsv';
 import EditXml from '../pages/EditXml';
 
+const extractDataToTxT = file => {
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader();
+
+		reader.onload = e => {
+			const content = e.target.result;
+			resolve(content);
+		};
+
+		reader.onerror = e => {
+			reject(new Error('Error al leer el archivo.'));
+		};
+
+		reader.readAsText(file);
+	});
+};
+
 const extractDataFromXML = file => {};
 
 const extractDataFromDOC = file => {};
 
-const extractDataFromCSV = file => {};
+const extractDataFromCSV = async file => {
+	const txt = await extractDataToTxT(file);
+	const lines = txt.split('\n');
+	const headers = lines
+		.shift()
+		.split(',')
+		.map(header => header.trim());
+
+	return lines.map(line => {
+		const values = line.split(',');
+		const valuesLength = values.length;
+		const obj = {};
+
+		headers.forEach((header, index) => {
+			if (index < valuesLength) {
+				obj[header] = values[index].trim();
+			}
+		});
+
+		return obj;
+	});
+};
 
 const extractDataFromTXT = file => {};
 
